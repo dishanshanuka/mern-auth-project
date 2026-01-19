a0b6c02a42d8da0470c6dad1951e9ddfa6a08e15
Dishan Shanuka <udshanuka11@gmail.com>
2026-01-19 18:25:45 +0530

    feat(user): implement authenticated profile retrieval and exclude sensitive fields

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -16,5 +17,6 @@ app.use(cors({credentials: true}));
 //API Endpoints
 app.get('/', (req, res) => res.send('API is running...'));
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
